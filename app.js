b36b8eee0f14ab19e5753caaf3cd4e037f5ddc0c
Jay Odedra <jayodedra9919@gmail.com>
2024-11-26 00:44:26 +0530

    Addind some JS

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ let hamburger = document.querySelector(".hamburger");
 let navMenu = document.querySelector(".nav-menu");
 let navBar = document.querySelector(".nav-bar");
 let navLinks = document.querySelector(".nav-link");
+let signUpBtn = document.querySelector(".sign-up-btn ");
+let signOverlay = document.querySelector(".sign-overlay");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
     navBar.classList.toggle("active");
 });
+
+signUpBtn.addEventListener("click", () => {
+    signOverlay.classList.toggle("active");
+    signOverlay.classList.contains("active") ?  signUpBtn.innerHTML = "Close" :  signUpBtn.innerHTML = "Sign Up";
+})
 
 
